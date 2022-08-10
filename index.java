232c27d567c9bc8a9f1116f839d2dac57ccef85a
Yokee-ss-hh <82704862+Yokee-ss-hh@users.noreply.github.com>
2022-08-10 18:06:12 +0530

    Update index.java

--- index.java
+++ index.java
@@ -82,6 +82,304 @@
         // https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
                 
         // Read why IEEE 754 is very important in floating point arithmetic operations (very important)
+                
+        --------------------------------------------------------------------------------------------------
+        --------------------------------------------------------------------------------------------------
+        // Widening Casting / Automatic Casting : byte -> short -> char -> int -> long -> float -> double
+        // Narrowing Casting : double -> float -> long -> int -> char -> short -> byte
+        
+        byte b = 12;
+
+        short s = b;
+
+        int i = s;
+
+        float f = i;
+
+        double d = f;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+
+        int i = 'U';
+        System.out.println(i); // prints 85
+        // In automatic conversion, smaller data types can fit in larger datatypes and conversion is taken care by java
+
+        double d = 12.43;
+
+        float f = (float)d;
+
+        long l = (long)f;
+
+        int i = (int)l;
+
+        short s = (short)i;
+
+        byte b = (byte)s;
+        
+        int x = 98;
+        char c = (char)x;
+        System.out.println(c); // prints b as o/p
+  
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+                
+        --------------------------------------------------------------------------------------------------------------------
+        --------------------------------------------------------------------------------------------------------------------
+        // Type Conversion cannot perform on boolean, char and String 
+        // We cannot cast above 6 datatypes into boolean
+        byte b = 88;
+
+        char c = (char)b;
+
+        System.out.println(c); // prints X
+
+        ---------------------------------------------------------------------------------------------------------------------
+         // As boolean takes just 1 bit, There  is no widening / automatic casting for boolean's
+         // Narrowing casting is not possible into booleans
+         // boolean b1 = (boolean)21; gives error as : Inconvertible types; cannot cast 'int' into 'boolean'
+         // Similarly for float, double, long , char, byte, short we gets the same error with respective names
+                
+        ----------------------------------------------------------------------------------------------------------------------
+        **********************************************************************************************************************
+         // Arithmetic Operators : +, -, *, /, % , ++, --
+         
+         byte b = 21;
+
+         short s = 231;
+
+         int i = 4321;
+
+         long l = 47236482394L;
+
+         float f = 432.3221f;
+
+         double d = 762.67823;
+
+         char c = 'G';
+
+         boolean bool = true;
+         
+         //<---------------- BIG NOTE : Acc to Oracle, Binary arithmetic operations on char and byte (and short) promote to int ----------------------->
+         // ASCII Table : <----------------https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html---------------------------->
+          
+         // Addition :
+
+         System.out.println(((Object)(b+b)).getClass());
+         System.out.println(((Object)(b+s)).getClass());
+         System.out.println(((Object)(b+i)).getClass());
+         System.out.println(((Object)(s+s)).getClass());
+         System.out.println(((Object)(s+i)).getClass());
+         System.out.println(((Object)(b+c)).getClass());
+         System.out.println(((Object)(s+c)).getClass());
+         System.out.println(((Object)(c+c)).getClass());    
+
+         // The above all lines gives o/p as  "class java.lang.Integer" as i said in line 168
+
+         System.out.println(21+'F');
+         System.out.println(((Object)(21+'F')).getClass()); // prints class java.lang.Integer
+         System.out.println('A'+'B'); prints 131
+         char c1 = 'A';
+         char c2 = 'B';
+         char c3 = 'A'+'B'; // prints '?'
+         System.out.println(c3); 
+         // Generally char+int = int acc to line 168, but we are storing in char, so type casting implicitly occurs 
+
+         // int + byte = int, if we want int + byte as byte , we need to cast into byte
+         // short + int = int, if we want int + short as short , we need to cast into short
+         // byte + short = short, if we want byte + short as byte, we need to cast into byte
+         // int + long = long, if we want int + long as int , we need to cast into int
+         // float + double = double, if we want float + double as float, we need to cast into float
+         // int + float = float
+         // int + double = double
+         // short + float = float
+         // byte + float = float
+         // short + double = double
+         // byte + double = double
+         
+
+         // Big datatype + small datatype = big datatype
+         // If we want to store big dt + small dt in big dt , then no error
+         // but, if we want to store big dt + small dt in small dt, we need to type cast
+         // Look at examples below for more:
+
+         // How int, float, byte, short, long, double works on char
+         byte b0 = (byte)(b + c);
+         // we need to narrow cast as byte memory is lower than that of char memory, byte-1 byte < char-2 bytes , b0 prints 92
+         int b1 = i + c;
+         // ascii value of G is 71 , so i+c = 4321+71 = 4392 stores in b1
+         short b3 = (short)(s+c); // prints 302 , in type casting order short comes before char, so we need to typecast it
+
+         long b4 = l + c;
+
+         float b5 = f + c;
+
+         double b6 = d + c;
+
+         // why only short and byte needs to be cast in above 6 lines
+         // Type Casting Order : byte > short > char > int > long > float > double
+         // AS byte, short are < char, we need to cast them but rest of them are not < char , so no cast required
+         // NOTE : While adding character to any other datatype, it is converted to it's ascii value before addition
+         ---------------------------------------------------------------------------------------------------------------------------
+         ---------------------------------------------------------------------------------------------------------------------------
+         // https://stackoverflow.com/questions/14342988/why-are-we-allowed-to-assign-char-to-a-int-in-java
+         // A char is simply an unsigned 16-bit number, so since it's basically a subset of the int type, the JVM can cast it without any ambiguity.
+         // char has a dual personality.
+         // It is a 16 bit unsigned integer numeric type, which makes assignment to int a very natural operation, involving widening primitive conversion, one of the conversions that the compiler will insert if needed without requiring an explicit cast.
+         // It is also the way characters are represented for I/O and String operations.
+                 
+         // char: The char data type is a single 16-bit Unicode character.
+         // It has a minimum value of '\u0000' (or 0) and a maximum value of '\uffff' (or 65,535 inclusive).
+
+         // We can assign an integer to char data type
+         int a = 'g';
+         System.out.println(a); // prints 103
+
+         // We can assign an integer
+         char c = 65535;
+         System.out.println(c); // prints Upper Case '?'
+
+         // Any integer above 65535 gives error in line 14
+
+         --------------------------------------------------------------------------------------------------------------------------
+         --------------------------------------------------------------------------------------------------------------------------
+         // Now Multiplication :
+                 
+         // Any arithmetic operations on byte,short and char results in int only acc to Oracle Corp..
+         System.out.println(((Object)(b-b)).getClass());
+         System.out.println(((Object)(b-s)).getClass());
+         System.out.println(((Object)(b-i)).getClass());
+         System.out.println(((Object)(s-s)).getClass());
+         System.out.println(((Object)(s-i)).getClass());
+         System.out.println(((Object)(b-c)).getClass());
+         System.out.println(((Object)(s-c)).getClass());
+         System.out.println(((Object)(c-c)).getClass());
+
+         // The above all gives output as : class java.lang.Integer
+
+
+         System.out.println(((Object)(l-i)).getClass());
+         System.out.println(((Object)(l-s)).getClass());
+         System.out.println(((Object)(l-b)).getClass());
+         // The above 3 lines gives class java.lang.Long
+         System.out.println(((Object)(f-l)).getClass());
+         System.out.println(((Object)(f-i)).getClass());
+         System.out.println(((Object)(f-s)).getClass());
+         System.out.println(((Object)(f-b)).getClass());
+         // The above 4 lines gives class java.lang.Float
+         System.out.println(((Object)(d-f)).getClass());
+         System.out.println(((Object)(d-l)).getClass());
+         System.out.println(((Object)(d-i)).getClass());
+         System.out.println(((Object)(d-s)).getClass());
+         System.out.println(((Object)(d-b)).getClass());
+         // The above 5 lines gives class java.lang.Double
+
+         -----------------------------------------------------------------------------------------------------------------------------
+         -----------------------------------------------------------------------------------------------------------------------------
+         // Now Multiplication : 
+                 
+         // Any arithmetic operations on byte,short and char results in int only acc to Oracle Corp..
+                 
+         System.out.println(((Object)(b*b)).getClass());
+         System.out.println(((Object)(b*s)).getClass());
+         System.out.println(((Object)(b*i)).getClass());
+         System.out.println(((Object)(s*s)).getClass());
+         System.out.println(((Object)(s*i)).getClass());
+         System.out.println(((Object)(b*c)).getClass());
+         System.out.println(((Object)(s*c)).getClass());
+         System.out.println(((Object)(c*c)).getClass());
+
+         // The above all lines prints class java.lang.Integer
+ 
+        System.out.println(((Object)(l*i)).getClass());
+        System.out.println(((Object)(l*s)).getClass());
+        System.out.println(((Object)(l*b)).getClass());
+        // The above 3 lines gives class java.lang.Long
+        System.out.println(((Object)(f*l)).getClass());
+        System.out.println(((Object)(f*i)).getClass());
+        System.out.println(((Object)(f*s)).getClass());
+        System.out.println(((Object)(f*b)).getClass());
+        // The above 4 lines gives class java.lang.Float
+        System.out.println(((Object)(d*f)).getClass());
+        System.out.println(((Object)(d*l)).getClass());
+        System.out.println(((Object)(d*i)).getClass());
+        System.out.println(((Object)(d*s)).getClass());
+        System.out.println(((Object)(d*b)).getClass());
+        // The above 5 lines gives class java.lang.Double
+
+        -----------------------------------------------------------------------------------------------------------------------------
+        -----------------------------------------------------------------------------------------------------------------------------
+        // Now Divsion and Modulo Operators : 
+                
+        System.out.println(21/11); // 1
+        System.out.println(-21/11); // -1
+        System.out.println(21/-11); // -1
+        System.out.println(0/11); // 0
+        System.out.println(21/0); // ArithmeticException
+        System.out.println(11/21); // 0
+        System.out.println(-11/21); // 0
+        System.out.println(11/-21); // 0
+
+        System.out.println(21%11); // 10
+        System.out.println(-21%11); // -10
+        System.out.println(21%-11); // 10
+        System.out.println(0%11); // 0
+        System.out.println(11%21); // 11
+        System.out.println(-11%21); // -11
+        System.out.println(11%-21); // 11
+        
+        // small num / big num = 0
+        // -num1 / num2 = -result
+        // num1 / -num2 = -result
+        // -num1 / -num2 = result
+
+        // small num % big num = small num
+        // -num1 % num2 = -result
+        // num1 % -num2 = result
+        // -num1 % -num2 = -result
+      
+        // Formula : mod(a,n) = a - {n * Floor(a/n)}
+
+        
+
+
+
+        
+
+        
+
+
+         
+                
+        ******************************************************************************************************************************
+        
+
+         
+
+
+               
+          
+                 
+         
+                 
+         
+                 
+         
+
+         
+         
+         
+
+
+        
+        
+          
                
         
         
